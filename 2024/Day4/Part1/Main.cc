4785c3cf50c765b77cbaeead824b1067d18117bd
Aidan Palmer <aidanhpalmer@gmail.com>
2024-12-04 18:58:58 -0700

    day 4 part 1 update

--- 2024/Day4/Part1/Main.cc
+++ 2024/Day4/Part1/Main.cc
@@ -1,38 +1,50 @@
 #include <fstream>
 #include <iostream>
-#include <sstream>
 #include <vector>
 
 using namespace std;
 
 int xmas(const vector<string>& words) {
     int total = 0;
-    int i, j;
+    size_t i, j;
 
     for (i = 0; i < words.size(); i++) {
-	for (j = 0; j < words[i].size(); j++) {
-	    string right, down, diag;
-	    try {
-		right = words[i].substr(j, 4);
-	    } catch (const out_of_range& e) {}
-	    
-            try {
-		down = words[i][j] + words[i + 1][j] + words[i + 2][j] + words[i + 3][j];
-	    } catch (const out_of_range& e) {}
+        for (j = 0; j < words[i].size(); j++) {
+            string right, left, down, up, down_right, down_left, up_right, up_left;//
 
-	     try {
-                diag = words[i][j] + words[i + 1][j + 1] + words[i + 2][j + 2] + words[i + 3][j + 3];
-            } catch (const out_of_range& e) {}
-	    if (right == "XMAS" || right == "SAMX") {
-		total++;
-	    }
-            if (down == "XMAS" || down == "SAMX") {
-                total++;
+            if (j + 3 < words[i].size()) {
+                right = words[i].substr(j, 4);
+                //cout << right << endl;
+
+                if (right == "XMAS" || right == "SAMX") {
+                    total++;
+                }
             }
-            if (diag == "XMAS" || diag == "SAMX") {
-                total++;
+            if (i + 3 < words.size()) {
+                down = words[i][j] + words[i + 1][j] + words[i + 2][j] + words[i + 3][j];
+
+                if (down == "XMAS" || down == "SAMX") {
+                    total++;
+                }
             }
-	}
+            if (i + 3 < words.size() && j + 3 < words[i].size()) {
+                down_right = words[i][j] + words[i + 1][j + 1] + words[i + 2][j + 2] + words[i + 3][j + 3];
+
+                if (down_right == "XMAS" || down_right == "SAMX") {
+                    total++;
+                }                
+            }
+
+            if (int(i) - 2 > 0 && int(j) - 2 > 0) {
+                //cout << i - 2 << endl << j - 2 << endl;
+                down_left = words[i][j] + words[i - 1][j - 1] + words[i - 2][j - 2] + words[i - 3][j - 3];
+
+                if (down_left == "XMAS" || down_left == "SAMX") {
+                    total++;
+                }
+            }
+
+        }
     }
     return total;
 }
