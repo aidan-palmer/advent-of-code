9cdeef466401293529aa71d5d7825dceefd5160b
Aidan Palmer <aidan.palmer@colostate.edu>
2024-12-13 15:54:28 -0700

    completed day 10 part 1

--- 2024/Day10/Part1/Main.cc
+++ 2024/Day10/Part1/Main.cc
@@ -2,33 +2,50 @@
 #include <iostream>
 #include <vector>
 #include <cstdint>
+#include <map>
 
 using namespace std;
 
-// Rotate matrix by 90 degrees
-vector<vector<uint>> rotate(vector<vector<uint>>& grid) {
-    vector<vector<uint>> matrix(grid[0].size());
+map<size_t, vector<size_t>> nines;
 
-    vector<uint> row(grid.size());
-    
+bool contains(size_t i, size_t j) {
+    for (size_t k = 0; k < nines[i].size(); k++) {
+        if (nines[i][k] == j) {
+            return true;
+        }
+    }
+    return false;
 }
 
-size_t path(vector<vector<uint>>& grid, size_t& i, size_t& j) {
-
+size_t path(vector<vector<int>>& grid, size_t i, size_t j, int prev) {
+    if (i >= grid.size() || j >= grid[0].size() || prev + 1 != grid[i][j]) {
+        return 0;
+    }
+    if (grid[i][j] == 9) {
+        if (contains(i, j)) {
+            return 0;
+        } else {
+            nines[i].push_back(j);
+            return 1;
+        }
+    }
+    return path(grid, i + 1, j, grid[i][j]) + path(grid, i - 1, j, grid[i][j]) + 
+        path(grid, i, j + 1, grid[i][j]) + path(grid, i, j - 1, grid[i][j]);
 }
 
-size_t score(vector<vector<uint>>& grid) {
+size_t score(vector<vector<int>>& grid) {
     size_t total = 0, i, j;
 
     for (i = 0; i < grid.size(); i++) {
         for (j = 0; j < grid[i].size(); j++) {
             if (grid[i][j] == 0) {
-                total += path(grid, i, j);
+                size_t p = path(grid, i, j, -1);
+                total += p;
+                nines.clear();
             }
         }
     }
-
-
+    return total;
 }
 
 int main(int argc, char **argv) {
@@ -43,10 +60,10 @@ int main(int argc, char **argv) {
         return 2;
     }
     string line;
-    vector<vector<uint>> grid;
+    vector<vector<int>> grid;
 
     while (getline(file, line)) {
-        vector<uint> nums;
+        vector<int> nums;
 
         for (size_t i = 0; i < line.size(); i++) {
             nums.push_back(line[i] - '0');
@@ -54,6 +71,6 @@ int main(int argc, char **argv) {
         grid.push_back(nums);
     }
 
-    //cout <<  << endl;
+    cout << score(grid) << endl;
     return 0;
 }
