4ddda13221eb582a4b4f3b5a4713405ef9f1b5be
Aidan Palmer <aidanhpalmer@gmail.com>
2024-12-14 21:37:52 -0700

    solved day 13 part 1

--- 2024/Day13/Part1/Main.cc
+++ 2024/Day13/Part1/Main.cc
@@ -3,7 +3,6 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
-#include <regex>
 #include <cstdint>
 
 using namespace std;
@@ -14,6 +13,24 @@ using namespace std;
 struct prize {
     uint btn_a_x, btn_a_y, btn_b_x, btn_b_y, prize_x, prize_y;
 };
+
+uint cheapest(prize& p) { 
+    vector<uint> results;
+
+    for (uint a = 1; a < 101; a++) {
+        for (uint b = 1; b < 101; b++) {
+            if (p.btn_a_x * a + p.btn_b_x * b == p.prize_x &&
+                p.btn_a_y * a + p.btn_b_y * b == p.prize_y) {
+                    results.push_back(a * ACOST + b * BCOST);
+                }
+        }
+    }
+    if (!results.empty()) {
+        sort(results.begin(), results.end());
+        return results[0];
+    }
+    return 0;
+}
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -27,21 +44,34 @@ int main(int argc, char **argv) {
         return 2;
     }
     string line;
-    string token;
+    vector<prize> prizes;
+    prize p;
+    ulong total = 0;
 
     while (getline(file, line)) {
-        stringstream stream(line);
-
-        
-
-        
+        // Make sure there are two empty lines at end of input file!
+        if (line.empty()) {
+            prizes.push_back(p);
+            continue;
+        }
+        size_t com = line.find(',');
+        // who needs regex?
+        if (line[7] == 'A') {
+            p.btn_a_x = stoi(line.substr(12, com - 12));
+            p.btn_a_y = stoi(line.substr(com + 4));
+        } else if (line[7] == 'B') {
+            p.btn_b_x = stoi(line.substr(12, com - 12));
+            p.btn_b_y = stoi(line.substr(com + 4));
+        } else if (line[7] == 'X') {
+            p.prize_x = stoi(line.substr(9, com - 9));
+            p.prize_y = stoi(line.substr(com + 4));
+        }
     }
-    /*
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    int total = 0;
-*/
 
-    //cout << total << endl;
+    for (size_t i = 0; i < prizes.size(); i++) {
+        total += cheapest(prizes[i]);
+    }
+
+    cout << total << endl;
     return 0;
 }
