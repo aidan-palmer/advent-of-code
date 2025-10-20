8fbc5a4896ac3b08367ced0ce64daac71f6e7a6c
Aidan Palmer <aidanhpalmer@gmail.com>
2025-10-20 14:26:54 -0600

    update

--- 2023/Day3/Part1/Main.cc
+++ 2023/Day3/Part1/Main.cc
@@ -8,8 +8,49 @@
 
 using namespace std;
 
-bool is_valid(int row, int start, int end, vector<string>& grid) {
+bool is_symbol(char c) {
+    if (!isdigit(c) && c != '.') {
+        return true;
+    }
+    return false;
+}
 
+bool is_valid(int row, int start, int end, vector<string>& grid) {
+    for (int i = start; i <= end; i++) {
+        // Up
+        if (row - 1 >= 0 && is_symbol(grid[row - 1][i])) {
+            return true;
+        }
+        // Down
+        if (row + 1 < grid.size() && is_symbol(grid[row + 1][i])) {
+            return true;
+        }
+        // Left
+        if (i == start && i - 1 >= 0 && is_symbol(grid[row][i - 1])) {
+            return true;
+        }
+        // Right
+        if (i == end && i + 1 < grid[row].size() && is_symbol(grid[row][i + 1])) {
+            return true;
+        }
+        // Right-Up
+        if (i == end && i + 1 < grid[row].size() && row - 1 >= 0 && is_symbol(grid[row - 1][i + 1])) {
+            return true;
+        }
+        // Right-Down
+        if (i == end && i + 1 < grid[row].size() && row + 1 < grid.size() && is_symbol(grid[row + 1][i + 1])) {
+            return true;
+        }
+        // Left-Up
+        if (i == start && i - 1 >= 0 && row - 1 >= 0 && is_symbol(grid[row - 1][i - 1])) {
+            return true;
+        }
+        // Left-Down
+        if (i == start && i - 1 >= 0 && row + 1 < grid.size() && is_symbol(grid[row + 1][i - 1])) {
+            return true;
+        }
+    }
+    return false;
 }
 
 int main(int argc, char **argv) {
@@ -54,9 +95,11 @@ int main(int argc, char **argv) {
         row++;
     }
     for (const auto& pair : parts) {
-        
+        //cout << pair.second << endl;
+        if (is_valid(pair.first[0], pair.first[1], pair.first[2], grid)) {
+            total += pair.second;
+        }
     }
-
-//    cout << total << endl;
+    cout << total << endl;
     return 0;
 }
