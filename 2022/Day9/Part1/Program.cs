9bb99fc76724fef992b8f1fb01acebffc1fb155b
Aidan Palmer <aidanhpalmer@gmail.com>
2025-11-11 19:45:26 -0700

    Started Day 9--hard one

---
+++ 2022/Day9/Part1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            throw new ArgumentException("Error: provide a text file as an argument.");
+        }
+        string line;
+        try
+        {
+            StreamReader sr = new StreamReader(args[0]);
+            line = sr.ReadLine();
+            while (line != null)
+            {
+
+                line = sr.ReadLine();
+            }
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+        
+    }
+}
